ba64c2d56d0888e96be4881d229e57e36c11bd04
ambreelmee <ambreelmee@gmail.com>
2023-12-07 09:17:25 +0100

    Still not working part 2 day 5

--- src/day5/part2.ts
+++ src/day5/part2.ts
@@ -55,49 +55,68 @@ const getLowestLocationNumberForSeedRange = (
 const getCategoryDestinationRange = (
   seedRange: SeedRange,
   categoriesMapping: Mapping[]
-) => {
-  const ranges = [];
-  for (const mapping of categoriesMapping) {
-    const seedRangeEnd = seedRange.start + seedRange.range;
-    const mappingEnd = mapping.sourceStart + mapping.range;
-    if (seedRange.start < mapping.sourceStart) {
-      if (seedRangeEnd >= mapping.sourceStart) {
-        const seedRangePreMapping = mapping.sourceStart - seedRange.start;
-        ranges.push({
-          start: seedRange.start,
-          range: seedRangePreMapping,
-        });
-        const subRange = seedRangeEnd - mapping.sourceStart;
-        const seedRangePostMapping =
-          seedRange.range - subRange - seedRangePreMapping;
-        if (seedRangePostMapping > 0) {
-          ranges.push({
-            start: mapping.sourceStart + mapping.range,
-            range: seedRangePostMapping,
-          });
-        }
-        ranges.push({
-          start: mapping.destinationStart,
-          range: subRange,
-        });
+): SeedRange[] => {
+  if (categoriesMapping.length === 0) {
+    return [seedRange];
+  }
+  const ranges = [] as SeedRange[];
+  const [firstMapping, ...otherMappings] = categoriesMapping;
+  const seedRangeEnd = seedRange.start + seedRange.range;
+  const mappingEnd = firstMapping.sourceStart + firstMapping.range;
+  if (seedRange.start < firstMapping.sourceStart) {
+    if (seedRangeEnd >= firstMapping.sourceStart) {
+      const subRange = seedRangeEnd - firstMapping.sourceStart;
+
+      ranges.push({
+        start: firstMapping.destinationStart,
+        range: subRange,
+      });
+      const seedRangePreMapping = firstMapping.sourceStart - seedRange.start;
+      ranges.push(
+        ...getCategoryDestinationRange(
+          {
+            start: seedRange.start,
+            range: seedRangePreMapping,
+          },
+          otherMappings
+        )
+      );
+      const seedRangePostMapping =
+        seedRange.range - subRange - seedRangePreMapping;
+      if (seedRangePostMapping > 0) {
+        ranges.push(
+          ...getCategoryDestinationRange(
+            {
+              start: firstMapping.sourceStart + firstMapping.range,
+              range: seedRangePostMapping,
+            },
+            otherMappings
+          )
+        );
       }
     }
-    if (seedRange.start >= mapping.sourceStart) {
-      if (seedRange.start < mappingEnd) {
-        const subRange = mappingEnd - seedRange.start;
-        if (subRange < seedRange.range) {
-          ranges.push({
-            start: seedRange.start + subRange,
-            range: seedRange.range - subRange,
-          });
-        }
-        ranges.push({
-          start:
-            mapping.destinationStart - mapping.sourceStart + seedRange.start,
-          range: Math.min(subRange, seedRange.range),
-        });
-      }
+  } else if (seedRange.start < mappingEnd) {
+    const subRange = mappingEnd - seedRange.start;
+    ranges.push({
+      start:
+        firstMapping.destinationStart -
+        firstMapping.sourceStart +
+        seedRange.start,
+      range: Math.min(subRange, seedRange.range),
+    });
+    const postMappingRange = seedRangeEnd - mappingEnd;
+    if (postMappingRange > 0) {
+      ranges.push(
+        ...getCategoryDestinationRange(
+          {
+            start: mappingEnd,
+            range: postMappingRange,
+          },
+          otherMappings
+        )
+      );
     }
   }
-  return ranges.length ? ranges : [seedRange];
+  console.log(ranges);
+  return ranges.length > 0 ? ranges : [seedRange];
 };
